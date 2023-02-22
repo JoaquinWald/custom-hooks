c03365b341e3e8b1addf2fceef3a2640117d9df5
JoaquinWald <joaquingrunwald@gmail.com>
2023-02-22 14:16:46 -0300

    First commit

---
+++ useTodo.js
@@ -0,0 +1,54 @@
+import { useEffect, useReducer } from "react";
+import { todoReducer } from "../08-useReducer/todoReducer";
+
+const init = () => {
+  return JSON.parse(localStorage.getItem('todos')) || [];
+};
+
+export const useTodo = () => {
+
+  const [todos, dispatch] = useReducer(todoReducer, [], init); //[] es el estado inicial.
+
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos))
+  }, [todos]);
+
+  const handleNewTodo = (todo) => {
+    const action = {
+      type: '[TODO] add todo',
+      payload: todo,
+    }
+    dispatch(action);
+  };
+
+  const handleDeleteTodo = (id) => {
+    dispatch({
+      type: '[TODO] Remove Todo',
+      payload: id,
+    });
+  };
+
+  const handleToggleTodo = (id) => {
+    dispatch({
+      type: '[TODO] Toggle Todo',
+      payload: id,
+    });
+  };
+
+  const todosCount = () => {
+    return todos.length;
+  }
+
+  const pendingTodosCount = () => {
+    return todos.filter(todo => !todo.done).length;
+  }
+
+  return {
+    todos,
+    handleNewTodo,
+    handleDeleteTodo,
+    handleToggleTodo,
+    todosCount,
+    pendingTodosCount,
+  }
+}
